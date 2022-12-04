89886d87b0a2c74b7f61bb294112186bb861c37a
MysteriousAcadia <mysteriousacadia@gmail.com>
2022-12-04 08:23:59 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,47 +4,87 @@ import Tree from "react-d3-tree";
 import { useState } from "react";
 
 const a = {
-  "Skill 1": {
-    _attributes: [{ attribute_name: "Name1", attribute_value: "vaule2" }],
-    "Skill 2": {
-      "Skill 4": {
-        _attributes: [{ attribute_name: "Name2", attribute_value: "vaule2" }],
+  "Python Basic": {
+    _attributes: [
+      { attribute_name: "Data Source", attribute_value: "Kaggle" },
+      { attribute_name: "No. Of Validators", attribute_value: "7" },
+      { attribute_name: "Python Language", attribute_value: "Yes" },
+      { attribute_name: "Pep8 Score", attribute_value: "> 60%" },
+      {
+        attribute_name: "Average duration of projects",
+        attribute_value: "> 2 months",
       },
-      _attributes: [{ attribute_name: "Name3", attribute_value: "vaule2" }],
-    },
-    "Skill 3": {
-      "Skill 5 🐳": {
-        _attributes: [{ attribute_name: "Name4", attribute_value: "vaule2" }],
-      },
-      "Skill 6": {
-        _attributes: [{ attribute_name: "Name5", attribute_value: "vaule2" }],
-      },
-      "Skill 7": {
-        "Skill 8": {
-          "Skill 9": {
-            _attributes: [
-              { attribute_name: "Name6", attribute_value: "vaule2" },
-            ],
+    ],
+    "SQL Basic": {
+      "SciKit ": {
+        PyTorch: {},
+        Keras: {},
+        _attributes: [
+          { attribute_name: "Data Source", attribute_value: "Kaggle" },
+          { attribute_name: "No. Of Validators", attribute_value: "6" },
+          { attribute_name: "Python Language", attribute_value: "Yes" },
+          { attribute_name: "Pep8 Score", attribute_value: "> 60%" },
+          { attribute_name: "SciKit Learn Library", attribute_value: "Yes" },
+          { attribute_name: "No. of projects", attribute_value: ">3" },
+          {
+            attribute_name: "Average duration of projects",
+            attribute_value: "> 2 months",
           },
-          "Skill 10": {
-            _attributes: [
-              { attribute_name: "Name7", attribute_value: "vaule2" },
-            ],
+        ],
+      },
+      TensorFlow: {
+        GCP: {},
+        _attributes: [
+          { attribute_name: "Data Source", attribute_value: "Kaggle" },
+          { attribute_name: "No. Of Validators", attribute_value: "6" },
+          { attribute_name: "SQL Language", attribute_value: "Yes" },
+          { attribute_name: "SQL Code Quality", attribute_value: "Above Good" },
+          { attribute_name: "SQL Basic Joins", attribute_value: "Yes" },
+          { attribute_name: "No. of projects", attribute_value: ">4" },
+          {
+            attribute_name: "Average duration of projects",
+            attribute_value: "> 2 months",
           },
+        ],
+      },
+      _attributes: [
+        { attribute_name: "Data Source", attribute_value: "Kaggle" },
+        { attribute_name: "No. Of Validators", attribute_value: "6" },
+        { attribute_name: "SQL Language", attribute_value: "Yes" },
+        { attribute_name: "SQL Code Quality", attribute_value: "Above Good" },
+        { attribute_name: "SQL Basic Joins", attribute_value: "Yes" },
+        { attribute_name: "No. of projects", attribute_value: ">4" },
+        {
+          attribute_name: "Average duration of projects",
+          attribute_value: "> 2 months",
         },
+      ],
+    },
+    "R Basic": {
+      "Shiny Library": {
+        AWS: {},
+        Azure: {},
+        _attributes: [
+          { attribute_name: "Data Source", attribute_value: "Kaggle" },
+          { attribute_name: "No. Of Validators", attribute_value: "6" },
+          { attribute_name: "SQL Language", attribute_value: "Yes" },
+          { attribute_name: "SQL Code Quality", attribute_value: "Above Good" },
+          { attribute_name: "SQL Basic Joins", attribute_value: "Yes" },
+          { attribute_name: "No. of projects", attribute_value: ">4" },
+          {
+            attribute_name: "Average duration of projects",
+            attribute_value: "> 2 months",
+          },
+        ],
       },
     },
-  },
-  "Skill 11": {
-    "Skill 12": {},
-    "Skill 13": {},
   },
 };
 
 const returnGraphObject = (name, objj) => {
   const obb = Object.keys(objj).filter((k) => k !== "_attributes");
   if (obb.length <= 0) {
-    return { name };
+    return { name, _attributes: objj["_attributes"] };
   }
   return {
     name: name,
@@ -55,7 +95,7 @@ const returnGraphObject = (name, objj) => {
     usl: { sadfasd: "d" },
   };
 };
-const kk = returnGraphObject("Msas", a);
+const kk = returnGraphObject("", a);
 
 function App() {
   const [selectedObject, setSelectedObject] = useState([]);
@@ -75,12 +115,12 @@ function App() {
         collapsible={false}
         data={kk}
       />
-      <div style={{ width: "800px" }}>
+      <div style={{ width: "800px", textAlign: "left" }}>
         <h1> Skill Detail:</h1>
         <br />
         {selectedObject.map(({ attribute_name, attribute_value }) => {
           return (
-            <div style={{ fontSize: "20px" }}>
+            <div style={{ fontSize: "20px", lineHeight: "40px" }}>
               <b>{attribute_name}</b> : {attribute_value}
               <br />
             </div>
